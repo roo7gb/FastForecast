bb0a5b82a52724fd25d563326d2b2aefa1f4f546
jr362 <jr362@uakron.edu>
2025-11-29 17:34:48 -0500

    Add .csv upload functionality

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -9,7 +9,7 @@
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import SeriesViewSet, DataPointViewSet, forecast_view, acf_view, decompose_view
+from .views import SeriesViewSet, DataPointViewSet, forecast_view, acf_view, decompose_view, upload_timeseries_view
 from .auth_views import signup_view, login_view, logout_view, user_view, csrf_view
 
 router = DefaultRouter()
@@ -21,6 +21,7 @@ urlpatterns = [
     path("forecast/", forecast_view, name="forecast"),
     path("acf/", acf_view, name="acf"),
     path("decompose/", decompose_view, name="decompose"),
+    path("upload/", upload_timeseries_view, name="upload series"),
     path("auth/signup/", signup_view, name="signup"),
     path("auth/login/", login_view, name="login"),
     path("auth/logout/", logout_view, name="logout"),
